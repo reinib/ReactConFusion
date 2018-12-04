08a17d858a9f0733925f4798cfd4cee3f53445ab
Brent Reininger <brent.reininger89@gmail.com>
2018-12-04 15:32:30 -0800

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ class DishDetail extends Component {
                     return (
                         <li key={comment.id}>
                             <p>{comment.comment}</p><br />
-                            <p>--{comment.author}, {comment.date}</p>
+                            <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                         </li>
                     );
                 })}
@@ -38,13 +38,15 @@ class DishDetail extends Component {
     render() {
         if (this.props.dish != null)
             return (
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
-                    </div>
-                    <div className="col-12 col-md-5 md-1">
-                        <h4>Comments</h4>
-                        {this.renderComments(this.props.dish.comments)}
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div className="col-12 col-md-5 md-1">
+                            <h4>Comments</h4>
+                            {this.renderComments(this.props.dish.comments)}
+                        </div>
                     </div>
                 </div>
             );
